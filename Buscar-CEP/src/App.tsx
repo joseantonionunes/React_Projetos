978abdb0460ca369e7811e0fd4f0d4be5c8cdc11
joseantonionunes <joseantonionunes2018@gmail.com>
2025-01-06 16:25:48 -0300

    Buscar CEP Fim

--- Buscar-CEP/src/App.tsx
+++ Buscar-CEP/src/App.tsx
@@ -1,22 +1,63 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import {BrowserRouter as Router} from 'react-router-dom'
 
-import Routes from "./routes";
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import api from './service/api';
+import './index.css'
 
-import GlobalStyle from './global'
 
 function App() {
 
+
+   const [input, setInput] = useState('');
+    const [cep, setCep] = useState({});
+
+    async function handleSearch(){
+      // 01310930/json/
+
+      if(input === '') {
+          alert("Preencha algum cep!")
+          return;
+      }
+
+      try{
+          const response = await api.get(`${input}/json`);
+          setCep(response.data)
+          setInput("")
+
+      }catch{
+          alert("Ops!! Erro ao buscar.")
+          setInput("")
+      }
+  }
+      
   return (
     <>
-      
-      <GlobalStyle />
-      <Router>
-        <Routes />
-      </Router>
-    
+      <section className='section'>
+            <h1 className='title'>Buscador CEP</h1>
+                <div className='div'>
+                    <input type="text" placeholder="Digite seu cep..."
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    />
+
+                    <button className='button' onClick={handleSearch}>
+                        <FiSearch size={25} color="#121212"/>
+                    </button>
+                </div>
+
+                {Object.keys(cep).length > 0 && (
+                    <main className='main'>
+                    <h2>CEP: {cep.cep}</h2>
+                    
+                    <span>{cep.logradouro}</span>
+                    <span>Complemento {cep.complemento}</span>
+                    <span>{cep.bairro}</span>
+                    <span>{cep.localidade} - {cep.uf}</span>
+
+                </main>
+                )}
+              
+        </section>
     </>
   )
 }
